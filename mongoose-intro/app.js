d4ec0e4beb763326ef1c1061f0864bde74b4cf49
Fábio Costa <fabiodanielmc@gmail.com>
2020-03-12 10:30:33 +0100

    2nd commmit

--- mongoose-intro/app.js
+++ mongoose-intro/app.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 
-mongoose.connect('mongodb://localhost/my_database', {
+mongoose.connect('mongodb://localhost/video', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -12,3 +12,25 @@ mongoose.connect('mongodb://localhost/my_database', {
 .catch((error)=>{
     console.log("Error", error);
 })
+
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema({
+    title: String,
+    year: Number,
+    rate: Number,
+    director: String
+});
+
+const MovieModel = mongoose.model("movies", MovieSchema);
+
+MovieModel
+    .find()
+    .select({_id: 0, title: 1, year: 1, rate: 1, director: 1})
+    .sort()
+    .then((movies)=>{
+        console.log(movies);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
